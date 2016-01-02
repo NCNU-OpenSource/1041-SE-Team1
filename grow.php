7a40d0d9e7596092f4ab84c713f6e7c8d319f491
dcw102213006 <s102213006@mail1.ncnu.edu.tw>
2016-01-02 22:24:00 +0800

    更新種植的同時跑出畫面

--- grow.php
+++ grow.php
@@ -14,45 +14,57 @@ require("conn.php");
 ?>
 <?php
 $id=(int)$_POST["id"];
-
-
-
-
-
-if ($id>0){
-    
-$sqlland = "select * from land where  id=$id;";//查詢是否已種作物
-$resultland=mysqli_query($conn,$sqlland);
-$rsland=mysqli_fetch_array($resultland);
-$status=$rsland['status'];
-
-$seed=mysqli_real_escape_string($conn,    $_POST['seed']   );
-if($seed==""){
-    header("Location:seed.php?id=$id");
+$nickname=$_SESSION['nk'];
+$sqlpower = "select * from player where  nickname='$nickname';";//查詢是否已種作物
+$resultpower=mysqli_query($conn,$sqlpower);
+$rspower=mysqli_fetch_array($resultpower);
+$power=$rspower['power'];
+if($power<10){
+    echo("您的體力不足!!!");
+    echo"<button   onclick=window.close()>取消</button>";
 }
-$sql2 = "select * from seed where  name='$seed';";
-$result2=mysqli_query($conn,$sql2);
-$rs2=mysqli_fetch_array($result2);
-$time=$rs2['time'];
-$name=$rs2['name'];
-$second=time()+($time*3600);
-$exp=$rs2['exp'];
+else{
 
 
-	if ($rs2) {
-	$sql = "update land set status='$seed',time='$time',seed='$name' , exp='$exp',ftime=$second where id=$id;";
-	mysqli_query($conn,$sql) or die("輸入錯誤1"); //執行SQL
+    if ($id>0){
+    
+        $sqlland = "select * from land where  id=$id;";//查詢是否已種作物
+        $resultland=mysqli_query($conn,$sqlland);
+        $rsland=mysqli_fetch_array($resultland);
+        $status=$rsland['status'];
+
+        $seed=mysqli_real_escape_string($conn,    $_POST['seed']   );
+        if($seed==""){
+            header("Location:seed.php?id=$id");
+        }
+        $sql2 = "select * from seed where  name='$seed';";
+        $result2=mysqli_query($conn,$sql2);
+        $rs2=mysqli_fetch_array($result2);
+        $time=$rs2['time'];
+        $name=$rs2['name'];
+        $second=time()+($time*3600);
+        $exp=$rs2['exp'];
+
+
+	    if ($rs2) {
+	    $sql = "update land set status='$seed',time='$time',seed='$name' , exp='$exp',ftime=$second where id=$id;";
+	    mysqli_query($conn,$sql) or die("輸入錯誤1"); //執行SQL
    
     
-	echo "成功種植!!!!";
-    $sql3 = "update seed set count=count-1 where name='$seed';";
-	mysqli_query($conn,$sql3) or die("輸入錯誤2"); //執行SQL
+	    echo "成功種植!!!!";
+        $sql3 = "update seed set count=count-1 where name='$seed';";
+	    mysqli_query($conn,$sql3) or die("輸入錯誤2"); //執行SQL
     
+        $sql4 = "update player set power=power-10 where nickname='$nickname';";
+	    mysqli_query($conn,$sql4) or die("輸入錯誤4"); //執行SQL
     
-    
- } 
+        } 
  
- } 
+   } 
+}
 ?>
+<script type="text/javascript">
+self.opener.location.reload(); //重新整理farm.php頁面
+</script>
 </body>
 </html>
