8977e0c05eb6c76b6841dce682e533f158ee2bec
dcw102213006 <s102213006@mail1.ncnu.edu.tw>
2016-01-05 22:42:51 +0800

    Create itemlist.php

---
+++ itemlist.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>商店</title>
+</head>
+
+<body>
+<style type="text/css">
+#shoplist{
+position:relative;
+
+}
+#buy{
+position:absolute;
+width:60px;
+left:420px;
+top:325px;
+}
+#apple{
+position:absolute;
+width:100px;
+left:100px;
+top:180px;
+}
+#rice{
+position:absolute;
+width:100px;
+left:300px;
+top:170px;
+}
+#tomato{
+position:absolute;
+width:90px;
+left:500px;
+top:180px;
+}
+#green_pepper{
+position:absolute;
+width:80px;
+left:100px;
+top:340px;
+}
+#grape{
+position:absolute;
+width:65px;
+left:300px;
+top:340px;
+}
+
+
+</style>
+<?php
+require("conn.php");
+?>
+
+<?php
+$id=(int)$_GET['id'];
+$sql = "select * from shop where id=$id;";
+$results=mysqli_query($conn,$sql);
+
+while ($rs=mysqli_fetch_array($results)) {
+
+?>
+
+<table>
+  <td><label>
+  <tr><form method="post" action="buy.php">
+   
+      <input type="hidden" name="id"  value="<?php echo $rs['id']; ?>" >
+     
+    </label></td>
+    <td><label>
+      <input  type="radio" name="seed" id="seed" value=<?php echo $rs['name']; ?>><?php echo $rs['name']; ?><?php echo $rs['time']; ?>小時
+    費用<?php echo $rs['money'];?>
+   
+   
+     收益<?php echo $rs['sold']; ?>
+   
+    
+      
+   </label></td> 
+    <td><label>
+    <input type="textbox" size=1 name="count"  />
+    </label></td>
+   <td><label>
+    <input type="submit" name="Submit" value="購買!" />
+    </label></td>
+
+ </tr>
+
+</form>
+<?php
+} 
+?>
+
